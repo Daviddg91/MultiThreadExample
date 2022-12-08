bb8e75f3d53a69f1d4196bcae72765edebf1339f
David <46385259+Daviddg91@users.noreply.github.com>
2022-12-09 00:50:35 +0100

    modified:   docker-compose.yml 	modified:   src/main/java/kafka/config/Producer.java 	modified:   src/main/resources/application.yml

--- src/main/java/kafka/config/Producer.java
+++ src/main/java/kafka/config/Producer.java
@@ -4,9 +4,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,9 +13,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import kafka.model.Email;
 
 
 
@@ -42,6 +37,7 @@ public class Producer {
     @Bean
     public Map<String, Object> producerConfiguration() {
         Map<String, Object> properties = new HashMap(kafkaProperties.buildProducerProperties());
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("spring.kafka.consumer.bootstrap-servers"));
 //        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 //        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return properties;
